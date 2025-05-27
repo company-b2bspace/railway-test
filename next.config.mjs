94d62ecd75be8786260d6bfc6908390f4d096131
Твоє ім'я <b2bspace.ee@gmail.com>
2025-05-27 15:44:06 +0300

    Configure output: standalone for next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs (або next.config.js) у чистому проекті railway-test
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true, // Залишаємо або встановлюємо за бажанням
+    output: "standalone", // 
+    experimental: {
+      instrumentationHook: true, // Залишаємо, ЯКЩО ви додавали файл instrumentation.ts для тестування глобальних помилок. Якщо ні - цю секцію experimental можна прибрати.
+    },
+  };
+  export default nextConfig; // Або module.exports = nextConfig; для .js
