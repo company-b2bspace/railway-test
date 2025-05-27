465ac496805c21f1d340e58a879d5252f69e48d0
Твоє ім'я <b2bspace.ee@gmail.com>
2025-05-27 15:56:39 +0300

    Update next.config.js (standalone) and package.json (start script & stable versions)

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-// next.config.mjs (або next.config.js) у чистому проекті railway-test
+// next.config.mjs (або next.config.js) у ЧИСТОМУ проекті railway-test
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, // Залишаємо або встановлюємо за бажанням
-    output: "standalone", // 
-    experimental: {
-      instrumentationHook: true, // Залишаємо, ЯКЩО ви додавали файл instrumentation.ts для тестування глобальних помилок. Якщо ні - цю секцію experimental можна прибрати.
-    },
-  };
-  export default nextConfig; // Або module.exports = nextConfig; для .js
+  reactStrictMode: true,
+  output: "standalone",
+};
+
+export default nextConfig; // Або module.exports = nextConfig; для .js-файлу
